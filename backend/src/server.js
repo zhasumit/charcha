f896a7b5e4d2f5b43b88dec7053863db4c941e3a
Sumit Jha <sumitjha1618@gmail.com>
2025-07-15 00:27:47 +0530

    base routes and controller setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,12 @@
 import express from "express";
 import "dotenv/config";
 
+import authRoutes from "./routes/auth.route.js"
+
 const app = express();
 const PORT = process.env.PORT;
 
-app.get("/api/auth/signup", (req, res) => {
-    res.send("Signup route");
-})
-
-app.get("/api/auth/login", (req, res) => {
-    res.send("Login route");
-})
-
-app.get("/api/auth/logout", (req, res) => {
-    res.send("Logout route");
-})
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server Up! @http://localhost:${PORT}`);
