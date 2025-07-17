af3f6cea11383e56c9945a6bd6286b87b50e34b0
Sumit Jha <sumit.jha@prismberry.com>
2025-07-17 10:53:25 +0530

    setting up mongodb and stream

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 
 import authRoutes from "./routes/auth.route.js"
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -10,4 +11,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server Up! @http://localhost:${PORT}`);
+    connectDB();
 })
