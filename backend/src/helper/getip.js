89f985fa552c3e6fbc3de2cc5f078b82b99ab12f
Sumit Jha <sumitjha1618@gmail.com>
2025-08-02 22:58:27 +0530

    reader for prod charcha 1.0

---
+++ backend/src/helper/getip.js
@@ -0,0 +1,13 @@
+import os from "os";
+
+export function getLocalIP() {
+    const interfaces = os.networkInterfaces();
+    for (const name of Object.keys(interfaces)) {
+        for (const iface of interfaces[name] || []) {
+            if (iface.family === "IPv4" && !iface.internal) {
+                return iface.address;
+            }
+        }
+    }
+    return "localhost"; // fallback
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,19 @@ import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import path from "path"
 
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
 import chatRoutes from "./routes/chat.route.js"
 
 import { connectDB } from "./lib/db.js";
+import { getLocalIP } from "./helper/getip.js";
 
 const app = express();
 const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
 
 
 // middlewares (functions before some routes or api)
@@ -26,7 +30,17 @@ app.use("/api/users", userRoutes)
 app.use("/api/chat", chatRoutes)
 
 
+// under the production go to the dist folder and get the index.html
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("/*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
+const ip = getLocalIP();
 app.listen(PORT, () => {
-    console.log(`Server Up! @http://localhost:${PORT}`);
+    console.log(`Server Up! @ http://${ip}:${PORT} (${process.env.NODE_ENV})`);
     connectDB();
 })
